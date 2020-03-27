62ae4bb48070c636f65fb14bbdd6a9ea44cb9376
Chagas Correa <vrcr@gft.com>
2020-03-27 11:20:47 -0300

    Primeiro teste com sucesso

--- src/test/java/com/trabalho/casadeshow/api/resources/CasaResourceTest.java
+++ src/test/java/com/trabalho/casadeshow/api/resources/CasaResourceTest.java
@@ -1,52 +1,52 @@
 package com.trabalho.casadeshow.api.resources;
 
-import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.trabalho.casadeshow.api.builders.CasasBuilders;
-import com.trabalho.casadeshow.api.model.Cadastrocasa;
-import com.trabalho.casadeshow.api.repository.Cadastroshow;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@RunWith(SpringJUnit4ClassRunner.class)
+//@AutoConfigureMockMvc
 public class CasaResourceTest {
 
 	@Autowired
 	public WebApplicationContext context;
+	
 	private MockMvc mockMvc;
 	
-	@MockBean
-	public Cadastroshow cadastroshow;
-	
+    @Mock
+	public CasaResources casaResources;
+		
 	@Before
 	public void setup() {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+		
 	}
-	
-	@Test
-	public void deveRetornarStatus200QuandoListar() throws Exception {
-	 List<Cadastrocasa> casas = Arrays.asList(CasasBuilders.umaCasa().retona(),
-			                                  CasasBuilders.umaCasa().comCodigo(2l).comNome("Arena").retona());
 	 
-	 Mockito.when(cadastroshow.findAll()).thenReturn(casas);
+	@Test
+	public void deveRetornarStatus200QuandoListar() throws Exception  {
 	 String url = "/api/casas";
 	 this.mockMvc.perform(get(url)).andExpect(MockMvcResultMatchers.status().isOk());
-	  
 	}
+
 }
 
